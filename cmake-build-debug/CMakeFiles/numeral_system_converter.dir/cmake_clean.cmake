23cba2ed0abe9ca63d4364658a66435c491ebfa3
DimYfantidis <axaiosyfan@gmail.com>
2021-05-24 10:34:13 +0300

    CMake files created.

---
+++ cmake-build-debug/CMakeFiles/numeral_system_converter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/numeral_system_converter.dir/main.cpp.obj"
+  "libnumeral_system_converter.dll.a"
+  "numeral_system_converter.exe"
+  "numeral_system_converter.exe.manifest"
+  "numeral_system_converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/numeral_system_converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/numeral_system_converter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for numeral_system_converter.
+# This may be replaced when dependencies are built.
